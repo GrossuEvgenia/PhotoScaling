db58eeee5a9002596fad462d586762691a804f4e
GrossuEvgenia <grossuevgenia@gmail.com>
2023-09-05 15:19:56 +0700

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.lr7_kg {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.lr7_kg to javafx.fxml;
+    exports com.example.lr7_kg;
+}
